d33c6e6838b002bb24ad4d6889588e5e6dbe55f0
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-10 17:03:24 +0200

    No dynamic linking

--- src/app.rs
+++ src/app.rs
@@ -13,7 +13,7 @@ pub fn create_app() -> App {
     }
 
     app.add_systems(Startup, add_player);
-    app.add_systems(Update, respond_to_keyboard);
+    app.add_systems(Update, (respond_to_mouse_button_press, respond_to_mouse_move));
 
     // Do not do update, as this will disallow to do more steps
     // app.update(); //Don't!
@@ -33,25 +33,28 @@ fn add_player(mut commands: Commands) {
     ));
 }
 
-fn respond_to_keyboard(
-    mut query: Query<(&mut Transform, &Player)>,
-    input: Res<ButtonInput<KeyCode>>,
+fn respond_to_mouse_button_press(
+    mut query: Query<&mut Transform, With<Player>>,
+    input: Res<ButtonInput<MouseButton>>,
 ) {
-    let (mut transform, _) = query.single_mut();
-    use bevy::input::keyboard::KeyCode;
-    if input.pressed(KeyCode::ArrowRight) {
-        transform.translation.x += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowLeft) {
-        transform.translation.x -= 1.0;
-    }
-    if input.pressed(KeyCode::ArrowUp) {
-        transform.translation.y += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowDown) {
-        transform.translation.y -= 1.0;
+    let mut player_position = query.single_mut();
+    if input.pressed(MouseButton::Left) {
+        // Do something
+        player_position.translation.x += 16.0;
     }
 }
+
+fn respond_to_mouse_move(
+    mut query: Query<&mut Transform, With<Player>>,
+    mut mouse_motion_event: EventReader<MouseMotion>,
+) {
+    for event in mouse_motion_event.read() {
+        let mut player_position = query.single_mut();
+        player_position.translation.x += event.delta.x / 20.0;
+        player_position.translation.y -= event.delta.y / 20.0;
+    }
+}
+
 
 #[cfg(test)]
 fn count_n_players(app: &mut App) -> usize {
@@ -111,65 +114,37 @@ mod tests {
     }
 
     #[test]
-    fn test_player_responds_to_right_arrow_key() {
+    fn test_player_responds_to_mouse_move() {
         let mut app = create_app();
         app.update();
         assert_eq!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
 
-        // Press the right arrow key, thanks Periwinkle
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowRight);
+        // Move the mouse
+        app.world_mut().send_event(bevy::input::mouse::MouseMotion {
+            delta: Vec2::new(100.0, 100.0),
+        });
 
         app.update();
         assert_ne!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
     }
 
     #[test]
-    fn test_player_responds_to_left_arrow_key() {
+    fn test_player_responds_to_mouse_button_press() {
         let mut app = create_app();
+        assert!(app.is_plugin_added::<InputPlugin>());
         app.update();
 
-        assert_eq!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
+        // Not moved yet
+        assert_eq!(Vec3::new(0.0, 0.0, 0.0), get_player_position(&mut app));
+
+        // Press the left mouse button
         app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowLeft);
+            .resource_mut::<ButtonInput<MouseButton>>()
+            .press(MouseButton::Left);
 
         app.update();
 
-        assert_ne!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
-    }
-
-    #[test]
-    fn test_player_responds_to_up_arrow_key() {
-        let mut app = create_app();
-        app.update();
-
-        assert_eq!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
-
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowUp);
-
-        app.update();
-
-        assert_ne!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
-    }
-
-    #[test]
-    fn test_player_responds_to_down_arrow_key() {
-        let mut app = create_app();
-        app.update();
-
-        assert_eq!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
-
-        // Press the key
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::ArrowDown);
-
-        app.update();
-
-        assert_ne!(get_player_coordinat(&mut app), Vec2::new(0.0, 0.0));
+        // Position must have changed now
+        assert_ne!(Vec3::new(0.0, 0.0, 0.0), get_player_position(&mut app));
     }
 }
