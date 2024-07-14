a70bdd203ad3f94208d15601d00bb062630104c9
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-14 13:02:17 +0200

    A player has a size, code by its Transform's scale

--- src/app.rs
+++ src/app.rs
@@ -87,7 +87,7 @@ fn get_player_position(app: &mut App) -> Vec2 {
 fn get_player_size(app: &mut App) -> Vec2 {
     let mut query = app.world_mut().query::<(&Transform, &Player)>();
     let (transform, _) = query.single(app.world());
-    transform.size.xy()
+    transform.scale.xy()
 }
 
 #[cfg(test)]
